866fd378371db44a958f92d64012ff0adf1641ce
vitasika <86631512+vitasika@users.noreply.github.com>
2021-11-13 14:01:26 +0000

    Deploying to gh-pages from @ vitasika/IceCream_Group-13_Team-project@1fa8c983514805438e3487605fe4f2eb204b156e 🚀

--- src.81db5069.js
+++ src.81db5069.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/IceCream_Group-13_Team-project/src.8d38075f.js.map
+//# sourceMappingURL=/IceCream_Group-13_Team-project/src.81db5069.js.map
