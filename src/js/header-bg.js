335e7a70c7716de650b94b0acc9923b924fe2d8b
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2021-11-18 11:30:46 +0200

    modal-border

--- src/js/header-bg.js
+++
@@ -1,6 +0,0 @@
-const elem = $('header');
-const scrolled = () => {
-   const threshold = $(document).scrollTop() > 50;
-   elem.toggleClass('scrolled', threshold);
-   };
-$(window).on({ scroll: scrolled });
